b40e3094466fda937dc25d3cb638f0f6dccab77d
Siming Fu <fusiming@ku.edu>
2020-10-13 17:47:05 -0500

    add menu page(only for play)

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
+var startBtn = document.getElementById('start');
+var menu = document.getElementById('menu screen');
 gameObjects = [] // array to iterate through during game loop
 let paddle = new Paddle(); // instantiate paddle
 let ball = new Ball(); // instantiate ball
 gameObjects.push(paddle); // add paddle to array
 gameObjects.push(ball); // add ball to array
-
-function animate() // main game loop occurs here
+var ani = function animate() // main game loop occurs here
 {
+    menu.style.display = 'none';
+
     requestAnimationFrame(animate); // waits until this animate is done and then calls it again
-    if (!paused)
+    ctx.clearRect(0, 0 , window.innerWidth, window.innerHeight); // clears the previous frame
+    for (let i = 0; i < gameObjects.length; i++) // iterate through game objects
     {
-        ctx.clearRect(0, 0 , window.innerWidth, window.innerHeight); // clears the previous frame
-        for (let i = 0; i < gameObjects.length; i++) // iterate through game objects
-        {
-            gameObjects[i].update(); // call update on each object
-        }
+        gameObjects[i].update(); // call update on each object
     }
 }
-
-animate(); // start the loop
+startBtn.onclick = ani; // start the loop
