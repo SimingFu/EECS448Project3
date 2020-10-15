007890b0220ba2e7ac6f1757abc3b2a49cefb955
Connor Sutton <connorsutton07@gmail.com>
2020-10-15 00:01:33 -0500

    experimental brick collision

--- main.js
+++ main.js
@@ -5,13 +5,33 @@ gameObjects = [] // array to iterate through during game loop
 let paddle = new Paddle(); // instantiate paddle
 let ball = new Ball(); // instantiate ball
 
-const BRICK_ROWS = 5;
-const BRICK_COLS = 8;
+const BRICK_ROWS = 8;
+const BRICK_COLS = 7;
 let brickset = new Brickset(BRICK_ROWS, BRICK_COLS, true); //instantiate brickset with number of rows and columns of bricks
 
 gameObjects.push(paddle); // add paddle to array
 gameObjects.push(ball); // add ball to array
 gameObjects.push(brickset);
+
+DetectCollisions = function()
+{
+    console.log(ball.radius, canvas.height);
+    if (ball.y_pos + ball.radius > canvas.height || ball.y_pos - ball.radius < 0) 
+    {
+        ball.vel.y *= -1;
+        console.log(ball.y, ball.vel.y);
+    }
+    let b_length = brickset.brick_length;
+    let b_height = brickset.brick_height;
+    for (let i = 0; i < brickset.bricks.length; i++)
+    {
+        let brick = brickset.bricks[i];
+        if(ball.x > brick.x && ball.xx < brick.x + b_length && ball.y > brick.y && ball.y < brick.y + b_height) 
+        {
+            ball.vel.y *= -1;
+        }
+    }
+}
 
 var ani = function animate() // main game loop occurs here
 {
@@ -20,11 +40,15 @@ var ani = function animate() // main game loop occurs here
     if (!paused)
     {
         ctx.clearRect(0, 0 , window.innerWidth, window.innerHeight); // clears the previous frame
+        gameObjects[1].detect_collisions(gameObjects[0], gameObjects[2]);
         for (let i = 0; i < gameObjects.length; i++) // iterate through game objects
         {
             gameObjects[i].update(); // call update on each object
-            DetectColiisions();
+            //gameObjects[]
+            //DetectCollisions();
         }
     }
 }
+
+
 startBtn.onclick = ani; // start the loop
--- scripts/ball.js
+++ scripts/ball.js
@@ -3,18 +3,17 @@ class Ball
     constructor()
     {
         this.x = canvas.width / 2; // initial position is middle of the screen
-        this.y = canvas.height / 2;
-        this.vel = {x: 0, y: 5} // initial velocities
+        this.y = 25;//canvas.height / 4;
+        this.vel = {x: 5, y: -5} // initial velocities
         this.radius = canvas.height / 40; // radius of ball dependent on screen size
     }
     update()
     {
-        let y_pos = this.y + this.vel.y;
-        let x_pos = this.x + this.vel.x;
-        this.detect_collisions(x_pos, y_pos)
+        //this.detect_collisions()
         this.x += this.vel.x; //increment x position based on velocity
         this.y += this.vel.y; //increment y position based on velocity
         this.draw(); // every time update is called, redraw the ball in its new position
+
     }
     draw()
     {
@@ -24,4 +23,45 @@ class Ball
         ctx.fill(); // fill in the circle
         ctx.closePath(); // end drawing
     }
+    detect_collisions(paddle, brickset)
+    {
+        let y_pos = this.y + this.vel.y;
+        let x_pos = this.x + this.vel.x;
+        if (y_pos + this.radius > canvas.height || y_pos - this.radius < 0) this.vel.y *= -1;
+        if (x_pos + this.radius > canvas.width || x_pos - this.radius < 0) this.vel.x *= -1;
+
+        let x_collide_distance = brickset.brick_length / 2 + this.radius;
+        let y_collide_distance = brickset.brick_height / 2 + this.radius;
+
+        for (let i = 0; i < brickset.bricks.length; i++)
+        {
+            let brick = brickset.bricks[i];
+            let b_cx = brick.x + (brickset.brick_length / 2);
+            let b_cy = brick.y + (brickset.brick_height / 2);
+
+            let x_vector = Math.abs(b_cx - x_pos);
+            let y_vector = Math.abs(b_cy - y_pos);
+
+            ctx.beginPath();
+            ctx.moveTo(b_cx, b_cy);
+            ctx.lineTo(x_pos, y_pos);
+            ctx.stroke();
+
+            //console.log(x_collide_distance, y_collide_distance);
+            if (x_vector <= x_collide_distance && y_vector <= y_collide_distance)
+            {
+                console.log("collide");
+                let prev_x = Math.abs(b_cx - this.x);
+                let prev_y = Math.abs(b_cy - this.y);
+                if (prev_x > x_collide_distance) this.vel.x *= -1;
+                else this.vel.y *= -1;
+                brickset.bricks.splice(i, 1);
+            }
+          
+
+        }
+        
+    }
 }
+//Normalize the paddle width from -π to π. At zero there should be no difference in the reflection.
+//sin(-π) is -1, sin(0) is 0, and sin(π) is 1.
