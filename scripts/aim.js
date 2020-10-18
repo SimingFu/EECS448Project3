795ba29d9d7778375693d4d1c3aef4600c1a0852
octavio sosa <octavio_sosa@icloud.com>
2020-10-18 02:29:39 -0500

    add mouse-sliding arrow-head

--- scripts/aim.js
+++ scripts/aim.js
@@ -17,13 +17,23 @@ class Aim {
   }
 
   update(startPos_x, startPos_y) {
+    //this.color = updateColor() //TODO
+    this.updateLine(startPos_x, startPos_y)
+    this.updateHead()
+  }
+
+  draw() {
+    this.drawLine()
+    this.drawHead()
+  }
+
+  updateLine(startPos_x, startPos_y) {
     this.startPos = {
       x: startPos_x,
       y: startPos_y
     }
 
     this.length = canvas.width*canvas.height*0.00004
-    this.color = '#00ddff'
 
     this.endPos = {
       x: this.startPos.x,
@@ -33,7 +43,11 @@ class Aim {
     this.lineWidth = canvas.width*canvas.height*0.000004
   }
 
-  draw() {
+  updateHead() {
+       
+  }
+
+  drawLine() {
     ctx.beginPath()
     ctx.lineWidth = this.lineWidth
     ctx.strokeStyle = this.color
@@ -41,5 +55,31 @@ class Aim {
     ctx.moveTo(this.startPos.x, this.startPos.y)
     ctx.lineTo(this.endPos.x, this.endPos.y)
     ctx.stroke()
+    ctx.closePath()
+  }
+
+  drawHead() {
+    let scale = 0.25
+    let leftLine = {
+      x: this.endPos.x - this.length*scale,
+      y: this.endPos.y + this.length*scale
+    }
+
+    let rightLine = {
+      x: this.endPos.x + this.length*scale,
+      y: this.endPos.y + this.length*scale
+    }
+
+    ctx.beginPath()
+    ctx.lineWidth = this.lineWidth
+    ctx.strokeStyle = this.color
+    ctx.lineCap = 'round'
+    ctx.lineJoin = 'round'
+    ctx.moveTo(this.endPos.x, this.endPos.y)
+    ctx.lineTo(leftLine.x, leftLine.y)
+    ctx.moveTo(this.endPos.x, this.endPos.y)
+    ctx.lineTo(rightLine.x, rightLine.y)
+    ctx.stroke()
+    ctx.closePath()
   }
 }
