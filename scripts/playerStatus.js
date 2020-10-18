d9b3fafb3a3e8a0f7920e45702877836443e7020
octavio sosa <octavio_sosa@icloud.com>
2020-10-17 22:05:04 -0500

    display score

--- scripts/playerStatus.js
+++ scripts/playerStatus.js
@@ -4,13 +4,18 @@ class PlayerStatus {
     this.statusObjects = []
 
     this.currentLives = 3
+    this.currentScore = 369
+    this.addLife_Score = 370
+
     this.playerLives = new Lives(this.currentLives)
-    this.score = 0
-    this.statusObjects = [this.playerLives]
+    this.playerScore = new Score(this.currentScore, this.addLife_Score)
+
+    this.statusObjects = [this.playerLives, this.playerScore]
   }
 
   update() {
     this.playerLives.update(this.currentLives)
+    this.playerScore.update(this.currentScore, this.addLife_Score)
   }
 
   draw() {
--- scripts/score.js
+++ scripts/score.js
@@ -1,21 +1,21 @@
 class Score {
   constructor(currentScore, addLife_Score) {
     this.currentScore = currentScore
-    this.addLife_Score = addLife_score
-    this.scale = 0.1
+    this.addLife_Score = addLife_Score
+    this.scale = 0.00004
     this.position = {
-      x: canvas.width - canvas.width*this.scale,
-      y: canvas.width*this.scale 
+      x: canvas.width - canvas.width*canvas.height*this.scale*0.25,
+      y: canvas.height*canvas.width*this.scale
     }
-    this.fontSize = canvas.height*0.1
+    this.fontSize = canvas.height*canvas.width*0.00004
   }
 
-  update(currentScore) {
+  update(currentScore, addLife_Score) {
     this.position = {
-      x: canvas.width - canvas.width*this.scale,
-      y: canvas.width*this.scale 
+      x: canvas.width - canvas.width*canvas.height*this.scale*0.25,
+      y: canvas.height*canvas.width*this.scale
     }
-    this.fontSize = canvas.height*0.1
+    this.fontSize = canvas.height*canvas.width*0.00004
     this.currentScore = currentScore
   }
 
@@ -23,7 +23,7 @@ class Score {
     //ctx.beginPath()
     ctx.font = this.fontSize + 'px serif'
     ctx.fillStyle = object_color
-    ctx.textAlign = 'center'
+    ctx.textAlign = 'right'
     ctx.fillText(this.currentScore + '', this.position.x, this.position.y)
     //ctx.closePath()
   }
