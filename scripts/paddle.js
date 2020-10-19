45ec875c9b634bb644a986c5f1f7d76efdfbc613
Connor Sutton <connorsutton07@gmail.com>
2020-10-18 20:36:10 -0500

    locked paddle in the canvas boundaries

--- scripts/paddle.js
+++ scripts/paddle.js
@@ -2,14 +2,17 @@ class Paddle // the thing the player controls
 {
     constructor()
     {
-        this.width = canvas.width / 6; // width of paddle
-        this.height = canvas.height / 30; // height of paddle
+        this.width = PADDLE_WIDTH; // width of paddle
+        this.height = PADDLE_HEIGHT; // height of paddle
         this.x = canvas.width / 2;  // initial x position
         this.y = canvas.height - this.height; // initial y position
     }
     update()
     {
-        if (mouse.x != undefined) this.x = mouse.x - (this.width / 2) // move paddle based on mouse position if it is defined (it is undefined until it moves)
+        if (mouse.x != undefined) 
+        {
+            this.x = Math.min(Math.max(mouse.x - (this.width / 2), 0), canvas.width - this.width); // move paddle based on mouse position if it is defined (it is undefined until it moves)
+        }
     }
     draw()
     {
